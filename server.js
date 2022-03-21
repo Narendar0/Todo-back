ab6ec0aaa1c1c744e1da27e9e06d9198ae1f9d17
narendraprasathnp7@gmail.com <narendraprasathnp7@gmail.com>
2022-03-21 13:05:00 +0530

    minor

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use('/api/tasks' , router)
 
 
-app.listen('8000' , err =>{
+app.listen(process.env.PORT ||'8000' , err =>{
     if(err)
     console.log(err)
     console.log('Server is running in PORT number : 8000')

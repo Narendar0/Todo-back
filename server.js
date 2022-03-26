ad805f56913759e2284fbffae84d492f87f3c0f0
narendraprasathnp7@gmail.com <narendraprasathnp7@gmail.com>
2022-03-26 19:56:22 +0530

    code

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ require('./models/db')
 
 app.use(express.json())
 app.use(cors(
-   { origin : ["https://localhost:8000/api","https://dashboard.heroku.com/apps/cap-todo-list" ]
-}
+  
 ))
 
 
 app.use('/api/tasks' , router)
 
-app.listen(process.env.PORT || '8000' , err => {
+app.listen('8000' , err => { 
     if(err) console.log(err)
     console.log('Server is Running at PORT number : 8000')
 })
